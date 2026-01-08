420eb36bb9ef4757f5112a349ad8b6d3b5a27aa6
Alissa <alissa.samara@my.mail.de>
2026-01-08 17:33:46 +0100

    Hello World on /

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
+import express from 'express';
+// import {} from "./routes";
+// import '#db';
+// import {} from "./middlewares"
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
 console.log('Hello World');
+
+app.listen(port, () => console.log(`\x1b[34mMain app listening at http://localhost:${port}\x1b[0m`));
